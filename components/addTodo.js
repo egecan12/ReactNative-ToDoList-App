1657d8a32f2720ee8435885b74ee25d46779ea49
egecan12 <egecan18k@gmail.com>
2020-05-03 17:21:17 -0400

    major fix up

---
+++ components/addTodo.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+
+export default function AddTodo({submitHandler}) {
+
+const [text, setText] = useState('');
+
+changeHandler = (val) => {
+
+setText(val);
+
+}
+
+  return (
+    <View style={styles.header}>
+      <TextInput placeholder='new goal' style={styles.input} onChangeText={changeHandler}/>
+      <Button onPress={ () => submitHandler(text)} title='add' color='#eb345e'/>
+    </View>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+      
+    marginBottom:10,
+    paddingVertical:6,
+    paddingHorizontal:7,
+    borderBottomColor: '#ddd'  },
+    borderBottomWidth:1,
+ 
+});
