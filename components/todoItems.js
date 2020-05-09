5100065b709faed5900e198ea4991ac3cc12c8e0
egecan12 <egecan18k@gmail.com>
2020-05-09 13:12:30 -0400

    delete icon has been added

--- components/todoItems.js
+++ components/todoItems.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet,TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet,TouchableOpacity} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 export default function TodoItem({item, pressHandler}) {
   return (
     <View >
-      <TouchableOpacity >
-         <Text style={styles.item} onPress={() => pressHandler(item.key)}>{item.text}</Text>
+      <TouchableOpacity onPress={() => pressHandler(item.key)}>
+        <View style={styles.item}>
+
+        <MaterialIcons name="delete" size={18} color='#333'/>
+        <Text style={styles.itemText}>{item.text}</Text>
+
+        </View>
+
       </TouchableOpacity>
     </View>
   );
@@ -22,7 +30,10 @@ const styles = StyleSheet.create({
         borderWidth:1,
         borderStyle:"dashed",
         borderRadius:10,
-
+        flexDirection:"row",
+    },
+    itemText:{
+      marginLeft:30,
 
     }
 
